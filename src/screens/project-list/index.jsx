cb0856fb6c64826175bd25bb6eeba6965c486fdf
hzyyy <812023671@qq.com>
2022-01-23 12:37:42 +0800

    增加debounce

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { cleanObject } from "utils/index"
+import { cleanObject, useMount, useDebounce } from "utils/index"
 import { List } from "./list"
 import { SearchPanel } from "./search-panel"
 import qs from 'qs'
@@ -13,29 +13,32 @@ export const ProjectListScreen = () => {
     personId: '',
   })
 
+  // 给param 添加debounce 防抖
+  const debounceParam = useDebounce(param, 1000)
+
   // 负责人
   const [ users, setUsers ] = useState([])
 
   // 项目列表
   const [ list, setList ] = useState([])
 
-  // 监听param 的变化，去请求项目数据
+  // 监听param / debounceParam 的变化，去请求项目数据
   useEffect(() => {
-    fetch(baseUrl + '/projects?' + qs.stringify(cleanObject(param))).then(async response => {
+    fetch(baseUrl + '/projects?' + qs.stringify(cleanObject(debounceParam))).then(async response => {
       if(response.ok) {
         setList(await response.json())
       }
     })
-  }, [param])
+  }, [debounceParam])
 
   // 只执行一次
-  useEffect(() => {
+  useMount(() => {
     fetch(baseUrl + '/users').then(async response => {
       if(response.ok) {
         setUsers(await response.json())
       }
     })
-  }, [])
+  })
   
   return <div>
     <SearchPanel users={users} param={param} setParam={setParam} />
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react"
+
 // 对传入的Object 进行数据处理
 export const cleanObject = (obj) => {
   const result = { ...obj }
@@ -11,4 +13,29 @@ export const cleanObject = (obj) => {
   })
 
   return result
+}
+
+// 封装hook，方法内的代码只执行一次
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback()
+  }, []);
+}
+
+// 封装hook，防抖
+// 不直接修改传入的value，而是通过useState 去修改
+export const useDebounce = (value, delay) => {
+  const [ debounceValue, setDebounceValue ] = useState(value)
+  
+  // 监听value 值的变化
+  useEffect(() => {
+    // 通过setDebounceValue，去更新state
+    const timeout = setTimeout(() => setDebounceValue(value), delay)
+    
+    // 每次在上一个useEffect 处理完后清理
+    return () => clearTimeout(timeout)
+  }, [value, delay]);
+
+  // 最终返回修改后的state 值
+  return debounceValue
 }
